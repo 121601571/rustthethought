233a1aad8d1b204ac84dc63294179b69421ffcf3
121601571 <48176182+121601571@users.noreply.github.com>
2021-02-08 16:58:23 +0800

    bor and un-bor example..

--- test001/src/main.rs
+++ test001/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
+    unbor();
+    bor();
+}
+fn unbor(){
+
+    let v1 = vec![1,2,3,4,5];
+    for i in v1.iter(){
+        println!("{}", *i);
+    }
+    // value not borrowed..
+    println!("{:?}", v1);
+
+}
+fn bor(){
+
+    let v1 = vec![1,2,3,4,5];
+    for i in v1{
+        println!("{}", i);
+    }
+    // value  borrowed..
+    //println!("{:?}", v1);
+
 }
